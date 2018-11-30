4c3a03c2dbede59c91ba3a00ff6139164f89261a
Matt Helsley <mhelsley@vmware.com>
2018-11-30 14:46:09 -0800

    objtool: Make recordmcount into an objtool subcmd
    Rather than a standalone executable merge recordmcount as a sub command
    of objtool. This is a small step towards cleaning up recordmcount and
    eventually saving ELF code with objtool.
    
    For the initial step all that's required is a bit of Makefile changes
    and invoking the former main() function from recordmcount.c because the
    subcommand code uses similar function arguments as main when dispatching.
    
    objtool ignores some object files that tracing does not, specifically
    those with OBJECT_FILES_NON_STANDARD Makefile variables. For this reason
    we keep the recordmcount_dep separate from the objtool_dep. When using
    objtool mcount we can also, like the other objtool invocations, just
    depend on the binary rather than the source the binary is built from.
    
    Subsequent patches will gradually convert recordmcount to use
    more and more of libelf/objtool's ELF accessor code. This will both
    clean up recordmcount to be more easily readable and remove
    recordmcount's crude accessor wrapping code.
    
    Signed-off-by: Matt Helsley <mhelsley@vmware.com>

--- tools/objtool/objtool.c
+++ tools/objtool/objtool.c
@@ -35,6 +35,7 @@ static const char objtool_usage_string[] =
 static struct cmd_struct objtool_cmds[] = {
 	{"check",	cmd_check,	"Perform stack metadata validation on an object file" },
 	{"orc",		cmd_orc,	"Generate in-place ORC unwind tables for an object file" },
+	{"mcount",	cmd_mcount,	"Construct a table of locations of calls to mcount. Useful for ftrace."},
 };
 
 bool help;
